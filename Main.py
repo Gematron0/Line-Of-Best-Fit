21ddd79508a1f4d3c0bd14789eca89d5e0572b24
Gematron2874 <88738641+Gematron0@users.noreply.github.com>
2025-02-10 19:39:26 +0100

    changes array A and B to array X and Y
    changes adday A and B to array X and Y for more easy reading

--- Main.py
+++ Main.py
@@ -1,18 +1,18 @@
 import LinRegMath
 
 def main():
-    ArrayA = [1.5, 2, 2.5, 3, 3.5]
-    ArrayB = [0.8, 1.3, 1.5, 1.9, 2.5]
-    MeanA = LinRegMath.Mean(ArrayA)
-    MeanB = LinRegMath.Mean(ArrayB)
-    SubArrayA = LinRegMath.NumSubMean(ArrayA, MeanA)
-    SubArrayB = LinRegMath.NumSubMean(ArrayB, MeanB)
-    BotFracArray = LinRegMath.TimesTwoArrays(SubArrayA, SubArrayB)
-    TopFracArray = LinRegMath.SquaredArray(SubArrayA)
+    ArrayX = [1.5, 2, 2.5, 3, 3.5]
+    ArrayY = [0.8, 1.3, 1.5, 1.9, 2.5]
+    MeanX = LinRegMath.Mean(ArrayX)
+    MeanY = LinRegMath.Mean(ArrayY)
+    SubArrayX = LinRegMath.NumSubMean(ArrayX, MeanX)
+    SubArrayY = LinRegMath.NumSubMean(ArrayY, MeanY)
+    BotFracArray = LinRegMath.TimesTwoArrays(SubArrayX, SubArrayY)
+    TopFracArray = LinRegMath.SquaredArray(SubArrayX)
     BotFrac = LinRegMath.SumAnArray(BotFracArray)
     TopFrac = LinRegMath.SumAnArray(TopFracArray)
     m = LinRegMath.FindM(BotFrac, TopFrac)
-    b = LinRegMath.FindB(m, MeanA, MeanB)
+    b = LinRegMath.FindB(m, MeanX, MeanY)
     print(f"y={m}x+{b}")
 
     
